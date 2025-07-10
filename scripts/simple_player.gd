6c31f9c99cbe9c5d7a0a4df815f20d4463e4b332
Morten Andersson <morten.andersson@gmail.com>
2025-07-11 01:51:11 +0200

    tweak style

--- scripts/simple_player.gd
+++ scripts/simple_player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+@export var SPEED = 1000.0
 
 const auto_move = true
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if auto_move:
 		velocity.x = SPEED
 	else:
