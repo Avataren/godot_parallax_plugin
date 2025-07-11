a8321b80575383b8b6d1310cb75225ba3ebccae0
Morten Andersson <morten.andersson@gmail.com>
2025-07-11 11:08:00 +0200

    vertical movement test

--- scripts/simple_player.gd
+++ scripts/simple_player.gd
@@ -1,17 +1,16 @@
 extends CharacterBody2D
-
 @export var SPEED = 1000.0
-
 const auto_move = true
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	if auto_move:
 		velocity.x = SPEED
-	else:
-		var direction = Input.get_axis("ui_left", "ui_right")
-		if direction:
-			velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+		position.y = sin(Time.get_ticks_msec() / 1000.0) * SPEED *10* delta
+	#else:
+		#var direction = Input.get_axis("ui_left", "ui_right")
+		#if direction:
+			#velocity.x = direction * SPEED
+		#else:
+			#velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
